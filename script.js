c9d42c4cc8ebb86c8f803c49d3e63be34348b5fb
Glitch (hello-webpage) <none>
2019-12-04 06:53:54 +0000

    🎮🍪 Checkpoint ./index.html:263690/218 ./script.js:263690/856 ./style.css:263690/72

--- script.js
+++ script.js
@@ -3,3 +3,32 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+const UPDATE_DELAY = 16;
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+function draw(ts) {
+
+  window.requestAnimationFrame(draw);
+}
+
+let updateTimer = null;
+let lastUpdateTime = Date.now();
+
+function update() {
+  const now = Date.now();
+  const timeDelta = now - lastUpdateTime;
+  lastUpdateTime = now;
+  
+  updateTimer = setTimeout(update, UPDATE_DELAY);  
+}
+
+function init() {
+  updateTimer = setTimeout(update, UPDATE_DELAY);
+  window.requestAnimationFrame(draw);
+}
+
+init();
