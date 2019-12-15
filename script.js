3c3ebc82765cb356abfebd610453b6c74b305bd5
Glitch (rose-viscount) <none>
2019-12-15 21:09:39 +0000

    💗🌶 Checkpoint ./lib/universe.js:263690/385 ./script.js:263690/1213

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /* global MainLoop, CryptoJS, alea */
-import { createSprite, updateSprite, drawSprite } from './lib/sprite.js';
 import xxhash from 'https://unpkg.com/xxhash-wasm/esm/xxhash-wasm.js';
+
+import { createSprite, updateSprite, drawSprite } from './lib/sprite.js';
+import { initUniverse } from './lib/universe.js';
 
 const seedrandom = Math.seedrandom;
 
@@ -10,45 +12,8 @@ const ctx = canvas.getContext('2d');
 const entities = [];
 
 async function init() {
-  // initGame();
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = '#fff';
-
-  const seed = 8675309;
-  
-  const t0 = performance.now();
-  const [fromIndex, fromRandom] = genRandom(seed, 1000);
-  const t1 = performance.now();
-  
-  console.log('perf', t0, t1, t1 - t0);
-  console.log('fromIndex', fromIndex);
-  console.log('fromRandom', fromRandom);
-  
-  for (const rand of Object.values(fromIndex)) {
-    const val = parseInt(rand, 16);
-    const x = (val & 0xff00) >> 8;
-    const y = val & 0x00ff;
-    ctx.fillRect(x, y, 1, 1);
-  }
-  
-}
-
-function genRandom(seed, maxIdx = 1000) {
-  const rng = new seedrandom(seed);
-  //const rng = new alea(seed);
-  
-  const indexToRandom = {};
-  const randomToIndex = {};
-  for (let idx = 0; idx < maxIdx; idx++) {
-    let val;
-    do {
-      val = Math.floor(rng() * 0xffff).toString(16).padStart(4, '0');
-    } while (val in randomToIndex);
-    indexToRandom[idx] = val;
-    randomToIndex[val] = idx;
-  }
-  return [indexToRandom, randomToIndex];
+  initGame();
+  initUniverse();
 }
 
 function initGame() {
