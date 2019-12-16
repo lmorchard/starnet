e990ae58ba33b1c5c2f94db1a08380e475afed0c
Glitch (rose-viscount) <none>
2019-12-16 07:55:03 +0000

    🐩😂 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,73 @@
+/* global MainLoop, CryptoJS, alea */
+import xxhash from 'https://unpkg.com/xxhash-wasm/esm/xxhash-wasm.js';
+
+import { createSprite, updateSprite, drawSprite } from './lib/sprite.js';
+import { initUniverse } from './lib/nodes/index.js';
+
+const seedrandom = Math.seedrandom;
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+const entities = [];
+
+async function init() {
+  initGame();
+  initUniverse();
+}
+
+function initGame() {
+  let margin = 40;
+  let width = 100;
+  let height = 100;
+  let maxx = 500;
+  let x = undefined;
+  let y = undefined;
+  
+  const incCoords = () => {
+    if (x === undefined) {
+      x = y = 0;
+    } else {
+      x += margin + width;
+      if (x >= maxx) {
+        x = 0;
+        y += margin + height;
+      }
+    }
+    return { x: x + margin, y: y + margin, width, height };
+  };
+  
+  [
+    createSprite({ ...incCoords(), seed: 'lmorchard', numPoints: 2 }),
+    createSprite({ ...incCoords(), seed: 'lmorchard', numPoints: 4 }),
+    createSprite({ ...incCoords(), seed: 'lmorchard', numPoints: 8 }),
+    createSprite({ ...incCoords(), seed: 'lmorchard', numPoints: 16 }),
+    createSprite({ ...incCoords(), seed: 'lmorchard' }),
+    createSprite({ ...incCoords(), seed: 'daemon' }),
+    createSprite({ ...incCoords(), seed: 'what' }),
+    createSprite({ ...incCoords(), seed: 'sprite' }),
+  ].forEach(entity => entities.push(entity));
+  
+  MainLoop.setUpdate(update).setDraw(draw).setEnd(end).start();
+}
+
+function update(delta) {
+  for (let idx = 0, len = entities.length; idx < len; idx++) {
+    updateSprite(entities[idx], delta)
+  }
+}
+
+function draw(interpolationPercentage) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let idx = 0, len = entities.length; idx < len; idx++) {
+    drawSprite(entities[idx], ctx, interpolationPercentage)
+  }
+}
+
+function end(fps, panic) {
+  if (panic) {
+    var discardedTime = Math.round(MainLoop.resetFrameDelta());
+  }
+}
+
+init().then().catch(console.error);
