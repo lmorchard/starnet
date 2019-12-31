fbd38ea5c383b2a5370a74014efc36e7f5f2a01e
Glitch (rose-viscount) <none>
2019-12-31 04:45:30 +0000

    Import of lmorchard/starnet

--- script.js
+++ script.js
@@ -1,123 +1,39 @@
-/* global MainLoop, CryptoJS, alea */
-import {
-  init as initWorld,
-  initState as initWorldState
-} from "./lib/ecs/index.js";
-import { PlayerState } from "./lib/ecs/player.js";
-import { Motion, Position } from "./lib/ecs/positionMotion.js";
-import {
-  Renderable,
-  CanvasContext,
-  RendererState,
-  ViewportFocus,
-  Shape,
-  drawFrameBuffer
-} from "./lib/ecs/viewportCanvas.js";
-import { GraphGroup } from "./lib/ecs/graph.js";
-import { Node } from "./lib/ecs/node.js";
-
+/* global dat */
+import { useRng } from "./lib/utils.js";
+import { initGame, startMainLoop } from "./lib/index.js";
 import { Universe } from "./lib/nodes/index.js";
-import { useRng, mkrng } from "./lib/utils.js";
-import { Planet, Region } from "./lib/nodes/planet.js";
+import { PlayerState } from "./lib/ecs/player.js";
 import * as Devices from "./lib/nodes/devices.js";
 import { RootNode } from "./lib/nodes/base.js";
+import { initCanvas } from "./lib/ecs/viewport/canvas/index.js";
+import { guiWorldState } from "./lib/debugGui.js";
+import { GraphLayoutState } from "./lib/ecs/graph.js";
+
+useRng(Math.seedrandom);
 
 const seed = "0000";
 
-let world;
-let worldState;
-let universe;
-let currentScene = [];
+const universe = new Universe({ addr: seed });
 
-async function init() {
-  useRng(Math.seedrandom);
+const { container, canvas, ctx } = initCanvas("#game");
 
-  initGraphics();
-  initGame();
-
-  MainLoop.setUpdate(update)
-    .setDraw(draw)
-    .setEnd(end)
-    .start();
-}
-
-let container, canvas, ctx;
-
-function initGraphics() {
-  container = document.querySelector("#game");
-  canvas = document.createElement("canvas");
-  container.appendChild(canvas);
-  ctx = canvas.getContext("2d");
-
-  /*
-  window.addEventListener( 'resize', () => {
-    canvasComponent.width = canvas.width = window.innerWidth
-    canvasComponent.height = canvas.height = window.innerHeight;
-  }, false );
-  */
-}
-
-function initGame() {
-  world = initWorld();
-  worldState = initWorldState(world, { container, canvas, ctx });
-
-  universe = new Universe({ addr: seed });
-
-  const playerState = worldState.getMutableComponent(PlayerState);
-  playerState.originNode = playerState.currentNode = universe.find({
-    type: Devices.Deck
-  });
-
-  changeSceneForNode(playerState.currentNode);
-}
-
-function update(delta) {
-  world.execute(delta, performance.now());
-}
-
-function draw(interpolationPercentage) {
-  drawFrameBuffer(worldState, interpolationPercentage);
-}
-
-function end(fps, panic) {
-  if (panic) {
-    var discardedTime = Math.round(MainLoop.resetFrameDelta());
-  }
-}
-
-function changeSceneForNode(sceneNode) {
-  for (let entity of currentScene) {
-    entity.remove();
-  }
-  currentScene = [];
-
-  const rng = mkrng(sceneNode.addr);
-  const contextNode = sceneNode.parent();
-
-  const sceneNodes = contextNode.walk({
-    map: ({ node, level }) => ({ node, level }),
+// HACK: defining this here, because importing RootNode in lib/index.js
+// seems to cause a lexical reference error that I haven't figured out yet
+const walkContextNode = contextNode =>
+  contextNode.walk({
     skipChildren: ({ node, level }) => level > 0 && node instanceof RootNode
   });
 
-  for (let { node } of sceneNodes) {
-    const entity = world
-      .createEntity()
-      .addComponent(Node, { node })
-      .addComponent(GraphGroup, { id: contextNode.addr })
-      .addComponent(Renderable)
-      .addComponent(Shape, { primitive: "node" })
-      .addComponent(Motion, { dx: 0, dy: 0 })
-      .addComponent(Position, {
-        x: rng() * canvas.width,
-        y: rng() * canvas.height
-      });
-    if (true && node.addr === sceneNode.addr) {
-      entity.addComponent(ViewportFocus);
-    }
-    currentScene.push(entity);
-  }
-}
+const { world, worldState, drawStats, gui } = initGame({
+  debug: true,
+  walkContextNode,
+  container,
+  canvas,
+  ctx
+});
 
-init()
-  .then()
-  .catch(console.error);
+const playerState = worldState.getMutableComponent(PlayerState);
+playerState.originNode = universe.find({ type: Devices.Deck });
+playerState.pendingCurrentNode = playerState.originNode;
+
+startMainLoop(world, worldState, drawStats);
