7475cab847883347736c3533d486666402bd6072
Les Orchard <me@lmorchard.com>
2021-10-06 21:24:04 -0700

    add a dynamic index listing for sketches

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,7 @@
 // Snowpack Configuration File
 // See all supported options: https://www.snowpack.dev/reference/configuration
+const path = require("path");
+const fs = require("fs");
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
@@ -7,6 +9,21 @@ module.exports = {
   mount: {},
   plugins: [
     ["snowpack-plugin-hash"],
+    [
+      "snowpack-plugin-ejs",
+      {
+        renderData: ({ filePath }) => {
+          const dirpath = path.dirname(filePath);
+          const dirname = path.basename(dirpath);
+          const paths = fs
+            .readdirSync(dirpath)
+            .filter((subpath) =>
+              fs.statSync(path.join(dirpath, subpath)).isDirectory()
+            );
+          return { dirname, paths };
+        },
+      },
+    ],
     // TODO https://www.npmjs.com/package/snowpack-plugin-assets
     // ["snowpack-plugin-assets", { assets: { from: [], to: "" } }],
   ],
