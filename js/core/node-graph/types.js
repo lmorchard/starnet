fff533b0d9af727eab17353775f39ddc43acb42d
Les Orchard <me@lmorchard.com>
2026-07-02 12:19:21 -0700

    Cascade substrate: declare kind on OperatorConfig typedef (lint fix) (#286)

--- js/core/node-graph/types.js
+++ js/core/node-graph/types.js
@@ -25,6 +25,7 @@
  * @typedef {Object} OperatorConfig
  * @property {string} name
  * @property {string} [filter]        - relay/counter: only process messages of this type
+ * @property {string} [kind]          - cascade: message type to propagate (default "pulse")
  * @property {string[]} [inputs]      - any-of / all-of: list of origin nodeIds to track
  * @property {string[] | null} [destinations] - relay/debounce: override outgoing destinations (null = broadcast)
  * @property {number} [period]        - clock / watchdog: emit / timeout every N ticks
